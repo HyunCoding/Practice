36790bb50f007baecd9a18b9f2d1125bebf7651f
HyunCoding <aa01080809571@gmail.com>
2021-07-30 22:15:52 +0900

    7/30

---
+++ js기초/practice.js
@@ -0,0 +1,16 @@
+const name = "Mike";
+const age = 30;
+
+const name1 = "Mike"
+const namw2 = 'Mike'
+const name3 = `Mike`
+
+const message1 = "i'm a boy.";
+const message2 = 'i\'m a boy.';
+
+const message3 = 'My name is ${name}';
+console.log(message3)
+
+
+
+

ccb83be42099fab4565359cc982672fedd633b14
HyunCoding <aa01080809571@gmail.com>
2021-09-12 01:45:42 +0900

    9/12 백준 파이썬 알고리즘

--- Python/파이썬 문법정리/pythonpractice.py
+++ Python/파이썬 문법정리/pythonpractice.py
@@ -1,2 +1,16 @@
-a = -2 % 60
-print(a)
+
+# a = []
+# for i in range(10):
+#     a.append(int(input()) % 42)
+# print(len(set(a)))
+
+
+input()
+a = list(map(int, input().split()))
+print(sum(a)/len(a)/max(a)*100)
+
+# 참고사항
+# list = [10,20,30]
+# sum(List) = 60
+# len(list) = 3
+# max(list) = 30
